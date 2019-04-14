4ece73412a5fdb15b15e6eecc8d65ac13e3dfd62
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-04-14 15:39:08 -0400

    age calculator

---
+++ ageCalculator.js
@@ -0,0 +1,17 @@
+//create an age calculator that tells you the amount of days you have been alive
+
+//prompt user for age
+var age = prompt("how old are you? ");
+var daysOld = age * 365.25;
+
+alert("you are roughly " + daysOld + " days old");
+
+
+
+//I need an age from the user to calculate
+
+//multiply that age by 365 on regular years
+//take into account leap  years
+//I need to multiply that age by 365
+//I need to calculate leap years
+//I need to alert the user the number of days he has been alive
