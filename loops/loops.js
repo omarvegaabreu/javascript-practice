9fdb97e27fafa53e806b166f553b4866fe73bfbf
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-04-15 14:51:53 -0400

    while loops

---
+++ loops/loops.js
@@ -0,0 +1,80 @@
+// var count = 0;
+
+// while (count <= 1000) {
+//   console.log(count);
+//   count += 100;
+  
+// }
+
+// var num = 1;
+
+// while (num <= 10) {
+//   console.log(num);
+//   num += 2;
+  
+// }
+
+// var num = 1
+
+// while (num <= 20) {
+//   if (num % 4 === 0) {
+//     console.log(num);  
+//   }
+//   num++;
+// }
+
+// var num = 100;
+
+// while (num < 150) { // infinite loop, NOT TO DO!!
+//   console.log(num + 1);
+//   num--;
+// }
+
+//print all the numbers between -10 and 19
+
+console.log("PRINT ALL THE NUMBERS BETWEEN -10 AND 19");
+
+var problem1 = -10;
+
+while (problem1 <= 19) {
+  console.log(problem1);
+  problem1++;
+}
+
+console.log("PRINT ALL EVEN NUMBERS BETWEEN 10 AND 40");
+
+var problem2 = 10;
+
+while (problem2 <= 40) {
+  if (problem2 % 2 === 0) {
+    console.log(problem2);
+  }
+  problem2++;
+}
+
+console.log("PRINT ALL ODD NUMBERS BETWEEN 300 AND 333")
+
+var problem3 = 300;
+
+while (problem3 <= 333) {
+  if (problem3 % 2 !== 0) {
+    console.log(problem3)
+  }
+  problem3++;
+}
+
+console.log("PRINT NUMBERS DIVISIBLE BY 5 AND 3 BETWEEN 5 AND 50")
+
+var problem4 = 5;
+
+while (problem4 <= 50) {
+  if (problem4 % 5 === 0 && problem4 % 3 === 0 ) {
+    console.log(problem4);
+  }
+  problem4++;
+}
+
+
+
+
+
