2ec588d627148877b86f8f032f9fdce6acc55af7
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-04-15 16:42:06 -0400

    playing with for loops

---
+++ for_loops/scripts.js
@@ -0,0 +1,34 @@
+console.log(" 'FOR LOOP' PRINT ALL THE NUMBERS BETWEEN -10 AND 19");
+
+for (var i = -10; i <= 19;i++ ) {
+  console.log(i);
+}
+
+console.log("'FOR LOOP:' PRING ALL EVEN NUMBERS BETTWEEN 10 AND 40");
+
+for (j = 10; j <= 40; j++) {
+  if (j % 2 === 0) {
+    console.log(j);
+  }
+}
+
+console.log("FOR LOOP PRINT ALL ODD NUMBERS BETWEEN 300 AND 333")
+
+for (k = 300; k <= 333; k++) {
+  if (k % 2 !== 0) {
+    console.log(k);
+  }
+}
+
+
+console.log("FOR LOOP PRINT NUMBERS DIVISIBLE BY 5 AND 3 BETWEEN 5 AND 50")
+
+for (l = 5; l <= 50; l++) {
+  if (l % 5 === 0 && l % 3 === 0) {
+    console.log(l)
+  }
+}
+
+
+
+
