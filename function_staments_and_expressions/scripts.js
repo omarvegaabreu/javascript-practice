37548f92c7e2ec31414a2934333e7a814e4bec10
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-04-17 14:41:58 -0400

    switch statement inside a function

---
+++ function_staments_and_expressions/scripts.js
@@ -0,0 +1,38 @@
+/***
+ * Function statement and expression
+ */
+
+ /**
+  * function declaration
+
+  * 
+  * function whatDoYouDo(argument, argument2) {
+
+  }
+  *
+  */
+
+  /**
+   * Function expression:
+   * you assign a function to a variable 
+   */
+
+
+var whatDoYouDo = function (job, firstName) {
+  
+  switch (job) {
+    case 'teacher':
+      return firstName + ' teaches people how to code'; 
+    case 'driver':
+      return firstName + ' drives uber part time';
+    case 'designer':
+      return firstName + ' builds awesome websites.';
+    default:
+      return firstName + ' does something else. ';     
+  }
+ 
+}
+console.log (whatDoYouDo('teacher', 'John'));
+console.log (whatDoYouDo('driver', 'Jane'));
+console.log (whatDoYouDo('designer', 'Jose'));
+console.log (whatDoYouDo('retired', 'Mike'));
