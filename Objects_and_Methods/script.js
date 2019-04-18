b172516e1ccbf4306b61d5f88b8a03538b0fdcd9
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-04-17 22:44:07 -0400

    this keyword

--- Objects_and_Methods/script.js
+++ Objects_and_Methods/script.js
@@ -4,6 +4,15 @@ var john = {
   birthYear: 1990,
   family: ['Jane', 'Mark', 'Bob', 'Emily'],
   job: 'Teacher',
-  isMarried: false
-  
+  isMarried: false,
+  // you can do a function inside an object
+  calAge: function () {
+    /***
+     * another usage of this keyword
+     * used to return a valua to age variable inside 
+     * the object.
+     */
+    this.age = 2018 - this.birthYear;
+  }
 };
+john.calAge();
