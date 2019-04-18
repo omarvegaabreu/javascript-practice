9c7bd78f008cb89dd7a944468d2031ede91729c1
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-04-17 20:02:39 -0400

    created object

--- objects/objects.js
+++ objects/objects.js
@@ -1,1 +1,11 @@
 
+var john = {
+  firstName: 'John',
+  lastName: 'Smith',
+  birthYear: 1990,
+  family: ['Jane', 'Mark', 'Bob', 'Emily'],
+  job: 'Teacher',
+  isMarried: false
+};
+
+console.log(john);
