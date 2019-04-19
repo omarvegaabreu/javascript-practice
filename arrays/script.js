d15a957aa7555685571fa2727bb1c7baf124e64e
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-04-19 15:50:00 -0400

    done with arrays for now

--- arrays/script.js
+++ arrays/script.js
@@ -2,7 +2,7 @@
  * Arrays
  */
 
-
+/*
  //initializing arrays
 var names = ['john', 'mark', 'jane'];
 var years = new Array(1990, 1969, 1948);//another way of doing arrays
@@ -25,6 +25,12 @@ john.push('designer');// push method to add element to variable, will change the
 var isDesigner = john.indexOf('designer') === -1 ? 'john is NOT a designer' : 'john IS a designer';
 //use of ternary in a variable to check if element is in variable.
 console.log(isDesigner);
+*/
+// var numbers = [22, 67, 33, 96, 88];
+
+// console.log(numbers[numbers.length]); 
+// it will print all the array numbers and it will print the number of elements in the array 5.
+// i was wrong lol 
 
 
 
