f1b1aa4c147817c22fcb7ff96750dd8c0d062192
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-04-21 18:33:28 -0400

    for each

--- array_iteration/script.js
+++ array_iteration/script.js
@@ -1,5 +1,21 @@
+
+/*
 var colors = ['red', 'orange', 'yellow', 'green'];
 
-for (var i = 0; i < colors.length; i++) {
-  alert(colors[i]);
-} 
+// for (var i = 0; i < colors.length; i++) {
+//   alert(colors[i]);
+// } 
+
+colors.forEach(function(colors){
+  console.log(colors);
+});
+*/
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var colors = ['red', 'orange', 'yellow', 'green'];
+
+numbers.forEach(function (colors) {
+  if (color % 3 === 0) {
+    console.log(color);
+  }
+});
