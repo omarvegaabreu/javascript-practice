3d069497f2ea9aff6a42cb00f6dea2bd2b02150c
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-04-21 19:35:27 -0400

    added for each in todo

--- toDoList/todoList.js
+++ toDoList/todoList.js
@@ -1,13 +1,35 @@
-var todos = ['buy a new turtle'];
+var todos = ["buy a new turtle"];
 
 var input = prompt("What would you like to do ?");
 
 while (input !== "quit") {
-      if (input === "list") {
-        console.log(todos);
-      } else if (input === "new") {
-        var newTodo = prompt('Enter new todo');
-        todos.push(newTodo);
-      }
+  if (input === "list") {
+    listTodos();
+  } else if (input === "new") {
+    addTodo();
+  } else if (input === "delete") {
+    //ask for index of todo to be deleted
+    deleteItem();
+  }
   input = prompt("What would you like to do ?");
-  };
+}
+console.log("OK YOU QUIT THE APP!!");
+
+function listTodos() {
+  console.log("*************");
+  todos.forEach(function(todo, i) {
+  console.log(i + ":" + todo);
+  console.log("*************");
+  });
+}
+
+function addTodo() {
+  var newTodo = prompt("Enter new todo");
+  todos.push(newTodo);
+}
+
+function deleteItem() {
+  var index = prompt("What index do you want to delete ?");
+  // delete that todo
+  todos.splice(index, 1);
+}
