609672ce92920f2d9837665a3898bdd931472071
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-05-10 23:28:50 -0400

    will finish tomorrow

--- functions/functions.js
+++ functions/functions.js
@@ -3,18 +3,37 @@
  */
 
 
+// function calculateAge(birthYear) {
+//   return 2018 - birthYear;
+// }
+
+// var ageJohn = calculateAge(1990);
+// var ageMike = calculateAge(1948);
+// var ageJane = calculateAge(1969);
+// const retirementAge = 65;
+// console.log(ageJane, ageMike, ageJohn)
+
+// // create a new function will will calculate years until retirement
+// const johnRetires = retirement(ageJohn,retirementAge);
+// const mikeRetires = retirement(ageMike,retirementAge);
+// const janeRetires = retirement(ageJane, retirementAge);
+
 function calculateAge(birthYear) {
   return 2018 - birthYear;
 }
+const ageJohn = calculateAge(1990);
+const ageMike = calculateAge(1948);
+const ageJane = calculateAge(1969);
 
-var ageJohn = calculateAge(1990);
-var ageMike = calculateAge(1948);
-var ageJane = calculateAge(1969);
-const retirementAge = 65;
-console.log(ageJane, ageMike, ageJohn)
+console.log(ageJohn, ageJane, ageMike);
 
-// create a new function will will calculate years until retirement
-const johnRetires = retirement(ageJohn,retirementAge);
-const mikeRetires = retirement(ageMike,retirementAge);
-const janeRetires = retirement(ageJane, retirementAge);
+function yearsUntilRetirement(year, firstName) {
+  const age = calculateAge(year);
+  const retirement? = 65 - age;
+  retirement < 65 ?
+}
+yearsUntilRetirement(1990,'John');
+yearsUntilRetirement(1948,'Mike');
+yearsUntilRetirement(1969,'Jane');
+
 
