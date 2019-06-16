e8f85894e58290b1dbde9aeee2c5fb23bae979d3
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-16 17:03:06 -0400

    javascript basics

--- challenge_5/script.js
+++ challenge_5/script.js
@@ -1,14 +1,29 @@
-const Mike = {
-  name: 'Mike',
+const john = {
+  name: 'John',
   lastName: 'Smith',
-  mass: 78,//kg
-  height: 1.69,//meters
-  bmi: function () {
-    this.bmi = this.mass / (this.height * this.height);
-    return this.bmi();
-    
+  bills: [124, 48, 268, 180],
+  tips:[], 
+  finalValue:[],
+  calcTip: function () {
+    this.tips=[];
+    this.finalValues = [];
+    for (var i = 0; i < this.bills.length; i++){
+      //determine the percentage base on tipping rules
+      let percentage;
+      const bill = this.bills[i];
+
+      if (bill < 50) {
+        percentage = .2;
+      } else if (bill >= 50 && bill < 200) {
+        percentage = .15;
+      } else {
+        percentage = .1;
+      }
+      this.tips[i] = bill * percentage;
+
+    }
   }
-  
-  
 }
-console.log(this.bmi);
+
+john.calcTip();
+console.log(john);
